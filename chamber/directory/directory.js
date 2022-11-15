c8aed2645014ebb50415d14375faa5f9647bd873
Taylor <chr16021@byui.edu>
2022-11-14 17:23:44 -0700

    additional js and json changes

--- chamber/directory/directory.js
+++ chamber/directory/directory.js
@@ -1,39 +1,45 @@
-const requestURL = 
-    'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+// fetch('directory.json').then(function(response){
+//     return response.json();
+// }).then(function(obj) {
+//     console.log(obj);
+// }).catch(function(error) {
+//     console.error('Something went wron with retrieving the directory')
+//     console.error(error);
+// });
+
+const requestImg = 'directory.json'
 const cards = document.querySelector('.cards');
 
-async function getProphets() {
-    const response = await fetch(requestURL);
+async function getDirectory() {
+    const response = await fetch('directory.json');
     const data = await response.json();
-    data.prophets.forEach(prophet => {displayProphets(prophet)})
+    data.businesses.forEach(business => {displaybusinesses(business)})
 }
 
-function displayProphets(prophet) {
+function displaybusinesses(business) {
 
     // Create elements to add to the document
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
-    // let h3 = document.createElement('h3');
     let portrait = document.createElement('img');
-    let birthDate = document.createElement('h3');
-    let birthPlace = document.createElement('h3');
+    let address = document.createElement('h3');
+    let phone = document.createElement('h3');
   
     // Change the textContent property of the h2 element to contain the prophet's full name
-    h2.textContent = `${prophet.name} ${prophet.lastname}`;
-    birthDate.textContent = `Date of Birth: ${prophet.birthdate}`;
-    birthPlace.textContent = `Place of Birth: ${prophet.birthplace}`;
+    h2.textContent = `${business.name}`;
+    address.textContent = `Business Adress: ${business.address}`;
+    phone.textContent = `Business Phone: ${business.phone}`;
   
     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-    portrait.setAttribute('src', prophet.imageurl);
-    portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname} ${prophet.order}`);
+    portrait.setAttribute('src', business.imageURL);
+    portrait.setAttribute('alt', `Business Logo ${business.name}`);
     // portrait.setAttribute('alt', ` Date of Birth ${prophet.birthdate}`);
     portrait.setAttribute('loading', 'lazy');
   
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
-    // card.appendChild(h3);
-    card.appendChild(birthDate);
-    card.appendChild(birthPlace);
+    card.appendChild(address);
+    card.appendChild(phone);
     card.appendChild(portrait);
   
     // Add/append the existing HTML div with the cards class with the section(card)
@@ -41,4 +47,4 @@ function displayProphets(prophet) {
   }
 
 
-getProphets();
+getDirectory();
